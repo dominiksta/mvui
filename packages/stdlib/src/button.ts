bacabaa1a1ccc45cb94a8bc3d339f0c0452c0e35
dominiksta <dominik.stahmer@posteo.de>
2023-02-03 21:26:04 +0100

    doc: small improvements to api docs

--- packages/stdlib/src/button.ts
+++ packages/stdlib/src/button.ts
@@ -2,8 +2,23 @@ import { Component, h, rx } from "@mvui/core";
 
 /**
  * This is a really cool button.
+ *
+ * @example
+ * ```typescript
+ * class Example extends Component {
+ *   render = () => [
+ *     Button.new('Click Me!'),
+ *   ]
+ * }
+```
  */
 export default class Button extends Component {
+
+  props = {
+    /** where does this go? */
+    text: new rx.Prop(''),
+  }
+
   #counter$ = new rx.Subject(0);
   #counterplusplus$ = this.#counter$.pipe(rx.map(el => el + 1));
 
